9b4380d26c7dd967b8383a49f17e5f0d2b670fe4
Kirji <buckiniv8@gmail.com>
2025-01-03 23:34:57 +0300

    New messages

--- bot.py
+++ bot.py
@@ -2,17 +2,32 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from datetime import datetime
 
 logging.basicConfig(level=logging.INFO)
 bot=Bot(token="7784426296:AAH_X3gsidh41U_S3NdfvAk8BhFFnK4hW6A")
 dp=Dispatcher()
+dp["started_at"]=datetime.now().strftime("%Y-%m-%d %H:%M")
 
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+@dp.message(Command("test1"))
+async def cmd_test1(message: types.Message):
+    await message.answer("Test 1")
+@dp.message(Command("test2"))
+async def cmd_test2(message: types.Message):
+    await message.answer("Test 2")
+@dp.message(Command("add_to_list"))
+async def cmd_add_to_list(message: types.Message, mylist: list[int]):
+    mylist.append(7)
+    await message.answer("Добавлено число 7")
+@dp.message(Command("show_list"))
+async def cmd_show_list(message: types.Message, mylist: list[int]):
+    await message.answer(f"Ваш список: {mylist}")
+@dp.message(Command("info"))
+async def cmd_info(message: types.Message, started_at: str):
+    await message.answer(f"Бот запущен {started_at}")
 
 async def main():
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, mylist=[1, 2, 3])
 
 if __name__ == "__main__":
     asyncio.run(main())
